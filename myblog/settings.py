517bc4bf508ac5d3d33064e7fa218a87fbd1cbca
Roberto Genchi <rob.genchi@gmail.com>
2019-04-03 00:48:32 +0000

    Ignored server venv-Uploaded settings for whitenoise and server

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1g*vja!=-!2mc)!gg)@o0!=!epwu*(@2q%2@1)2^6((ze4u04f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['robooze.eu.pythonanywhere.com',]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
