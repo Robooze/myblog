28cf684870f6e43fd7a7c931dbe0cbf3c79578c9
Roberto Genchi <rob.genchi@gmail.com>
2019-04-04 18:25:12 +0200

    added basic article detail functionality

--- articles/urls.py
+++ articles/urls.py
@@ -5,4 +5,5 @@ app_name = 'articles'  # Namespacing this urls file
 
 urlpatterns = [
     re_path(r'^$', views.article_list, name='list'),  # giving a url name which we can link to later on
+    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail, name='detail'),
 ]
