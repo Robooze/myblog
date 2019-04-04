cad35e826f5f82fc952858611d3210ab887b189d
Roberto Genchi <rob.genchi@gmail.com>
2019-04-04 22:40:17 +0200

    Redirected the homepage to the article list. Removed previous homepage

--- myblog/urls.py
+++ myblog/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import re_path, include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import articles
 
 # I am going to implement paths with REGEX (re_path)
 urlpatterns = [
@@ -9,7 +10,7 @@ urlpatterns = [
     re_path(r'^accounts/', include('accounts.urls')),
     re_path(r'^articles/', include('articles.urls')),
     re_path(r'^about/$', views.about),
-    re_path(r'^$', views.homepage, name='home'),
+    re_path(r'^$', articles.views.article_list, name='home'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- myblog/views.py
+++ myblog/views.py
@@ -1,11 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Callback functions fired when relative URL's are requested
-def homepage(request):  
-    # return HttpResponse('homepage')
-    return render(request,'homepage.html')
-
 
 def about(request):
     # return HttpResponse('about')
