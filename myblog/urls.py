44258742e215fa52b32f7e357c70b56a59896176
Roberto Genchi <rob.genchi@gmail.com>
2019-04-04 21:09:49 +0200

    added statics and a base layout for templates

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import re_path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # I am going to implement paths with REGEX (re_path)
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^articles/', include('articles.urls')),
     re_path(r'^about/$', views.about),
-    re_path(r'^$', views.homepage),
+    re_path(r'^$', views.homepage, name='home'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
