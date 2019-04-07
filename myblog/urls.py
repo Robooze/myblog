23d8bf11e8b51fd4631d44b53169a69ef3440009
Roberto Genchi <rob.genchi@gmail.com>
2019-04-07 15:56:49 +0200

    added bio page

--- myblog/urls.py
+++ myblog/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^accounts/', include('accounts.urls')),
     re_path(r'^articles/', include('articles.urls')),
-    re_path(r'^about/$', views.about),
+    re_path(r'^about/$', views.about, name='about'),
     re_path(r'^$', articles.views.article_list, name='home'),
 ]
 
